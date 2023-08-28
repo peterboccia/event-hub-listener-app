f959bfd83c28e67fcc364a924a94809c69c87996
Pietro Boccia <pietro.boccia@avanade.com>
2023-08-28 18:14:34 +0200

    Fixed some deployment options

--- EventHubTestApp/Services/History.cs
+++ EventHubTestApp/Services/History.cs
@@ -2,10 +2,10 @@
 {
     internal class History
     {
-        public string EventHubConnectionString { get; set; }
-        public string EventHubName { get; set; }
-        public string EventHubConsumerGroup { get; set; }
-        public string CheckpointConnectionString { get; set; }
-        public string CheckpointContainerName { get; set; }
+        public string EventHubConnectionString { get; set; } = "";
+        public string EventHubName { get; set; } = "";
+        public string EventHubConsumerGroup { get; set; } = "";
+        public string CheckpointConnectionString { get; set; } = "";
+        public string CheckpointContainerName { get; set; } = "";
     }
 }
